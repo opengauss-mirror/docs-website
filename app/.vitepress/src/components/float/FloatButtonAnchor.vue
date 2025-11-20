c7603d945f6ce717518d255a00de923ee0b08a96
luckyasme <807254037@qq.com>
2025-11-20 10:35:10 +0800

    feat: 增加返回顶部、移动端反馈组件

---
+++ app/.vitepress/src/components/float/FloatButtonAnchor.vue
@@ -0,0 +1,309 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, watch, nextTick, computed } from 'vue';
+import { useData, useRoute } from 'vitepress';
+import { isClient, OIcon, ODialog, OScroller } from '@opensig/opendesign';
+
+import IconAnchor from '~icons/feedback/icon-anchor.svg';
+
+import type { AnchorItemT } from '@/@types/type-common';
+import { getOffsetTop, getScrollRemainingBottom, isElementVisible } from '@/utils/element';
+import { scrollIntoView } from '@/utils/scroll-to';
+import { useViewStore } from '@/stores/view';
+import { useLocale } from '@/composables/useLocale';
+
+const route = useRoute();
+const viewStore = useViewStore();
+const { hash } = useData();
+const { t } = useLocale();
+
+const anchorScrollerRef = ref();
+const showDlg = ref(false);
+const anchorData = ref<AnchorItemT[]>([]);
+const activeVal = ref(hash.value);
+
+const showAnchor = computed(() => {
+  return anchorData.value.length > 0;
+});
+
+const onShow = () => {
+  if (showAnchor.value) {
+    showDlg.value = true;
+  }
+}
+
+// -------------------- 显示弹窗滚动到当前选中锚点位置 --------------------
+const scrollToIndicatorPosition = async () => {
+  if (!isClient) {
+    return;
+  }
+
+  await nextTick();
+  await nextTick();
+  
+  const anchorScroller = anchorScrollerRef.value?.getContainerEl() as HTMLElement;
+  if (!anchorScroller) {
+    return;
+  }
+
+  const el = anchorScroller.querySelector('.dlg-anchor-item-link.is-active') as HTMLElement;
+  if (!el) {
+    return;
+  }
+
+  if (!isElementVisible(el, anchorScroller, el.offsetHeight)) {
+    scrollIntoView(el, anchorScroller);
+  }
+};
+
+watch(showDlg, (val) => {
+  if (val) {
+    scrollToIndicatorPosition();
+  }
+});
+
+// -------------------- 更新 active  --------------------
+const updateActive = async (hashVal: string) => {
+  if (hashVal === '') {
+    activeVal.value = '';
+    return;
+  }
+
+  const href = decodeURIComponent(hashVal);
+  if (anchorData.value.some((item) => item.href === href)) {
+    activeVal.value = href;
+  }
+};
+
+// -------------------- 获取锚点数据 --------------------
+const getAnchorData = () => {
+  if (!isClient) {
+    return;
+  }
+
+  anchorData.value = [];
+  const markdownBody = document.querySelector('.markdown-body');
+  if (!markdownBody) {
+    return;
+  }
+
+  Array.from(markdownBody.querySelectorAll('h2')).forEach((element) => {
+    anchorData.value.push({
+      title: element.innerText.replace(/[\u200B-\u200D\uFEFF]/g, ''),
+      href: `#${element.id.replace('user-content-', '')}`,
+    });
+  });
+};
+
+watch(
+  () => route.path,
+  async () => {
+    anchorData.value = [];
+    await nextTick();
+    getAnchorData();
+    updateActive(hash.value);
+  },
+  {
+    immediate: true,
+  }
+);
+
+// -------------------- 滚动更新锚点 --------------------
+const onScroll = () => {
+  if (viewStore.isScrolling) {
+    return;
+  }
+
+  const scrollContainer = document.querySelector<HTMLElement>('#app > .o-scroller > .o-scroller-container');
+  if (!scrollContainer) {
+    return;
+  }
+
+  const contentDom = document.querySelector('.markdown-body');
+  if (!contentDom) {
+    return;
+  }
+
+  const scrollRemainingBottom = getScrollRemainingBottom(scrollContainer);
+  const distances: Array<{ hash: string; top: number }> = [];
+  for (const item of anchorData.value) {
+    const target = contentDom.querySelector<HTMLElement>(item.href.replace('#', '#user-content-'));
+    if (!target) {
+      continue;
+    }
+
+    const top = getOffsetTop(target, scrollContainer);
+
+    if (top < 110 || (scrollRemainingBottom < 100 && top >= 110)) {
+      distances.push({
+        hash: item.href,
+        top,
+      });
+    }
+  }
+
+  let active = '';
+  if (distances.length) {
+    if (scrollRemainingBottom < 10) {
+      active = distances[distances.length - 1].hash;
+    } else if (scrollRemainingBottom < 110) {
+      const overItems = distances.filter((item) => item.top >= 110);
+      if (overItems.length) {
+        const average = Math.round(110 / overItems.length);
+        const item = overItems.find((_, i) => (i + 1) * average < 110 - scrollRemainingBottom);
+        if (item) {
+          active = item.hash;
+        }
+      }
+    } else {
+      const max = distances.reduce((prev, cur) => (prev.top > cur.top ? prev : cur));
+      active = max.hash;
+    }
+  }
+
+  updateActive(active);
+};
+
+onMounted(() => {
+  const scrollContainer = document.querySelector<HTMLElement>('#app > .o-scroller > .o-scroller-container');
+  if (scrollContainer) {
+    scrollContainer.addEventListener('scroll', onScroll);
+  }
+});
+
+onBeforeUnmount(() => {
+  const scrollContainer = document.querySelector<HTMLElement>('#app > .o-scroller > .o-scroller-container');
+  if (scrollContainer) {
+    scrollContainer.removeEventListener('scroll', onScroll);
+  }
+});
+
+// -------------------- 监听hash改变 --------------------
+const scrollIntoTarget = async (hashVal: string) => {
+  if (viewStore.isScrolling) {
+    return;
+  }
+
+  viewStore.isScrolling = true;
+  if (hashVal) {
+    const contentDom = document.querySelector('.markdown-body');
+    if (contentDom) {
+      const hash = decodeURIComponent(hashVal).slice(1);
+      const target =
+        contentDom.querySelector<HTMLElement>(`#user-content-${hash}`) ||
+        contentDom.querySelector<HTMLElement>(`#${hash}`) ||
+        contentDom.querySelector<HTMLElement>(`[name='${hash}']`);
+      const scrollContainer = document.querySelector<HTMLElement>('#app > .o-scroller > .o-scroller-container');
+      if (target && scrollContainer) {
+        await scrollIntoView(target, scrollContainer, 60);
+      }
+    }
+  }
+
+  viewStore.isScrolling = false;
+};
+
+onMounted(() => {
+  setTimeout(async () => {
+    await scrollIntoTarget(activeVal.value);
+    if (!anchorData.value.some((item) => item.href === activeVal.value)) {
+      onScroll();
+    }
+  }, 300);
+});
+
+watch(
+  () => hash.value,
+  async (newHash) => {
+    updateActive(newHash);
+    await scrollIntoTarget(newHash);
+    if (!anchorData.value.some((item) => item.href === activeVal.value)) {
+      onScroll();
+    }
+  }
+);
+</script>
+
+<template>
+  <div class="float-btn-anchor" :class="{ 'float-btn-anchor-hidden': !showAnchor }" @click="onShow">
+    <OIcon class="icon-anchor">
+      <IconAnchor />
+    </OIcon>
+  </div>
+
+  <ODialog
+    v-model:visible="showDlg"
+    :style="{ '--dlg-edge-gap': '16px 24px', '--dlg-body-padding': '24px 24px 16px', '--dlg-radius': '4px' }"
+    :scroller="false"
+    main-class="float-dlg-anchor"
+  >
+    <div class="dlg-anchor-title">{{ t('docs.anchorTip') }}</div>
+    <OScroller ref="anchorScrollerRef" class="dlg-anchor-scroller" size="small" show-type="never" :disabled-x="true">
+      <div v-for="item in anchorData" :key="item.href" class="dlg-anchor-item" @click="showDlg = false">
+        <a :href="item.href" target="_self" class="dlg-anchor-item-link" :class="{ 'is-active': activeVal === item.href }">{{ item.title }}</a>
+      </div>
+    </OScroller>
+  </ODialog>
+</template>
+
+<style lang="scss">
+.float-dlg-anchor {
+  --dlg-width: 100%;
+
+  .dlg-anchor-title {
+    margin-bottom: 10px;
+    font-weight: 500;
+    text-align: center;
+    @include display3;
+  }
+
+  .dlg-anchor-scroller {
+    max-height: 50vh;
+  }
+
+  .dlg-anchor-item {
+    @include text2;
+  }
+
+  .dlg-anchor-item-link {
+    color: var(--o-color-info2);
+    display: block;
+    padding: 12px 16px;
+    border-radius: var(--o-radius-xs);
+  }
+
+  .dlg-anchor-item-link.is-active {
+    color: var(--o-color-primary1);
+    background-color: var(--o-color-fill3);
+    font-weight: 500px;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.float-btn-anchor {
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  margin-bottom: 8px;
+  background-color: var(--o-color-fill2);
+  border-radius: var(--o-radius-xs);
+  box-shadow: var(--o-shadow-2);
+  color: var(--o-color-info1);
+  cursor: pointer;
+}
+
+.float-btn-anchor-hidden {
+  opacity: 0;
+  cursor: default;
+}
+
+[data-o-theme='dark'] {
+  .float-btn-anchor {
+    border: 1px solid var(--o-color-control4-light);
+  }
+}
+
+.icon-anchor {
+  font-size: var(--o-font_size-h2);
+}
+</style>
---
+++ app/.vitepress/src/components/float/FloatButtonItems.vue
@@ -0,0 +1,238 @@
+<script setup lang="ts">
+import { ref, computed, markRaw, type PropType } from 'vue';
+import { OIcon, OPopup, ODivider, OLink } from '@opensig/opendesign';
+
+import IconSmile from '~icons/app/icon-smile.svg';
+import IconHeadset from '~icons/feedback/icon-headset.svg';
+import IconFAQ from '~icons/feedback/icon-faq.svg';
+import IconForum from '~icons/feedback/icon-forum.svg';
+
+import FeedbackSlider from './FeedbackSlider.vue';
+
+import { useLocale } from '@/composables/useLocale';
+
+import { useAppearance } from '@/stores/common';
+import { useThrottleFn } from '@vueuse/core';
+import FeedbackSliderDocs from './FeedbackSliderDocs.vue';
+
+defineProps({
+  source: {
+    type: String as PropType<'home' | 'docs'>,
+    default: 'home',
+  },
+});
+
+const { t, locale } = useLocale();
+
+const isDark = computed(() => {
+  return useAppearance().theme === 'dark' ? true : false;
+});
+
+// -------------------- 文档满意度 --------------------
+const feedbackRef = ref();
+const showPopup = ref(false); // 显示满意度调研弹窗
+const showInput = ref(false); // 显示弹窗中输入框
+// 鼠标进入图标区域
+const onMouseEnter = () => {
+  showPopup.value = true;
+};
+// 鼠标离开图标区域
+const onMouseLeave = () => {
+  showPopup.value = false;
+};
+// 滑动滑块
+const changeSlider = (v: boolean) => {
+  showInput.value = v;
+};
+// 关闭弹窗
+const closeFeedbackPopup = (v: boolean) => {
+  showInput.value = v;
+  showPopup.value = v;
+};
+
+// -------------------- 文档反馈 --------------------
+const issuebackRef = ref();
+
+const floatData = ref([
+  {
+    img: markRaw(IconForum as Object),
+    id: 'forum',
+    text: computed(() => t('feedback.forum')),
+    textMb: computed(() => t('feedback.forumHelp')),
+    tip: computed(() => t('feedback.forumTip')),
+    link: import.meta.env.VITE_SERVICE_FORUM_URL,
+  },
+  {
+    img: markRaw(IconFAQ as Object),
+    id: 'faq',
+    text: computed(() => t('feedback.faq')),
+    tip: '',
+    link: `/${locale.value}/docs/common/faq/faq.html`,
+  },
+]);
+</script>
+
+<template>
+  <div class="nav-box" :class="isDark ? 'dark-nav' : ''">
+    <div class="nav-item" id="feedback" @mouseenter="onMouseEnter" @mouseleave="useThrottleFn(onMouseLeave, 300)">
+      <OIcon ref="feedbackRef" class="icon-smile">
+        <IconSmile />
+      </OIcon>
+
+      <OPopup
+        :visible="showPopup"
+        position="rb"
+        :target="feedbackRef"
+        :auto-hide="showInput ? false : true"
+        wrapper="#feedback"
+        body-class="popup-feedback"
+        :offset="24"
+        trigger="hover"
+      >
+        <FeedbackSlider v-if="source === 'home'" :show="showInput" @close="closeFeedbackPopup" @input="changeSlider" />
+        <FeedbackSliderDocs v-else @close="closeFeedbackPopup" @input="changeSlider" />
+      </OPopup>
+    </div>
+
+    <ODivider :style="{ '--o-divider-gap': '12px' }" />
+
+    <div class="nav-item">
+      <OIcon ref="issuebackRef" id="issueback">
+        <IconHeadset />
+      </OIcon>
+
+      <OPopup
+        position="rb"
+        :target="issuebackRef"
+        wrapper="#issueback"
+        :body-class="`popup-issueback ${locale === 'en' ? 'popup-issueback-en' : ''}`"
+        :offset="24"
+        trigger="hover"
+      >
+        <OLink v-for="item in floatData" :key="item.id" :href="item.link" target="_blank" class="popup-item">
+          <OIcon><component :is="item.img"></component> </OIcon>
+
+          <div class="text">
+            <p class="text-name">
+              {{ item.text }}
+            </p>
+
+            <p v-if="item.tip" class="text-tip">{{ item.tip }}</p>
+          </div>
+        </OLink>
+      </OPopup>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.o-icon {
+  font-size: 24px;
+}
+.nav-box {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 12px;
+  background-color: var(--o-color-fill2);
+  border-radius: var(--o-radius-xs);
+  box-shadow: var(--o-shadow-2);
+}
+.nav-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--o-color-info1);
+  cursor: pointer;
+
+  @include hover {
+    color: var(--o-color-primary1);
+  }
+}
+
+:deep(.o-link-label) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+:deep(.o-popup) {
+  cursor: default;
+
+  .o-popup-wrap {
+    box-shadow: none;
+  }
+
+  .popup-feedback {
+    padding: 16px 30px;
+    background-color: var(--o-color-fill2);
+    box-shadow: var(--o-shadow-2);
+    border-radius: var(--o-radius-xs);
+    width: 360px;
+    top: 12px;
+  }
+
+  .popup-item {
+    .o-icon {
+      font-size: var(--o-font_size-h1);
+      color: var(--o-color-info1);
+    }
+  }
+
+  .popup-issueback {
+    padding: 24px;
+    background-color: var(--o-color-fill2);
+    border-radius: var(--o-radius-xs);
+    box-shadow: var(--o-shadow-2);
+    width: 224px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+
+    .popup-item {
+      padding: 0;
+
+      .o-link-main {
+        display: flex;
+        align-items: flex-start;
+        color: var(--o-color-info1);
+      }
+
+      @include hover {
+        & .text .text-name {
+          color: var(--o-color-primary1);
+        }
+      }
+
+      & ~ .popup-item {
+        margin-top: 12px;
+      }
+
+      .text {
+        margin-left: 8px;
+        text-align: left;
+        align-self: center;
+
+        .text-name {
+          font-size: var(--o-font_size-tip1);
+          line-height: 22px;
+          font-weight: 600;
+          a {
+            color: var(--o-color-info1);
+          }
+        }
+        .text-tip {
+          font-size: var(--o-font_size-tip2);
+          line-height: 18px;
+          color: var(--o-color-info2);
+          margin-top: 4px;
+        }
+      }
+    }
+  }
+
+  .popup-issueback-en {
+    width: 260px;
+  }
+}
+</style>
