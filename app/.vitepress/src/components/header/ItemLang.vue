59b14983f54bda12d4a19ebf82761248c7881fa4
luckyasme <807254037@qq.com>
2025-11-20 14:16:57 +0800

    feat: header相关组件

---
+++ app/.vitepress/src/components/header/ItemLang.vue
@@ -0,0 +1,178 @@
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+import { useRouter } from 'vitepress';
+import { OIcon, ODropdown, ODropdownItem } from '@opensig/opendesign';
+
+import IconLocale from '~icons/app/icon-locale.svg';
+
+import { useScreen } from '@/composables/useScreen';
+import { useLocale } from '@/composables/useLocale';
+import { isPageExist } from '@/api/api-common';
+
+const router = useRouter();
+const { locale } = useLocale();
+const { lePadV } = useScreen();
+
+interface LangType {
+  id: string;
+  label: string;
+}
+
+const langList = ref<LangType[]>([
+  { id: 'zh', label: '中文' },
+  { id: 'en', label: 'English' },
+]);
+
+async function changeLanguage(newlang: string) {
+  if (locale.value === newlang) {
+    return;
+  }
+  
+  const { pathname, search } = window.location;
+  const newHref = pathname.replace(`/${locale.value}/`, `/${newlang}/`);
+  if (await isPageExist(newHref)) {
+    router.go(newHref + search);
+  } else {
+    window.location.href = `/${newlang}/`;
+  }
+}
+
+const getLang = (lang: String, simple?: boolean) => {
+  return lePadV.value ? (lang === 'zh' ? '中文' : 'EN') : lang === 'zh' ? (simple ? '中' : '简体中文') : simple ? 'EN' : 'English';
+};
+</script>
+
+<template>
+  <div v-if="!lePadV" :class="[langList.length <= 1 ? 'hide-lang' : 'header-lang', 'lang-box']">
+    <ODropdown trigger="hover" options-wrapper=".lang-box" optionPosition="top" option-wrap-class="dropdown">
+      <div class="info-wrap">
+        <OIcon class="icon">
+          <IconLocale />
+          <div :class="['locale-tag', { 'is-en': locale === 'en' }]">{{ getLang(locale, true) }}</div>
+        </OIcon>
+      </div>
+
+      <template #dropdown>
+        <ODropdownItem v-for="item in langList" @click="changeLanguage(item.id)" :key="item.id" :class="['list', { 'is-active': locale === item.id }]">
+          {{ getLang(item.id) }}
+        </ODropdownItem>
+      </template>
+    </ODropdown>
+  </div>
+
+  <div v-else :class="langList.length <= 1 ? 'hide-lang' : 'mobile-change-language'">
+    <span v-for="item in langList" :key="item.id" :class="{ active: locale === item.id }" @click.stop="changeLanguage(item.id)">
+      {{ getLang(item.id) }}
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hide-lang {
+  display: none;
+}
+
+.header-lang {
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  .info-wrap {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .icon {
+      font-size: var(--o-icon_size_control-m);
+      position: relative;
+      color: var(--o-color-info1);
+
+      &:hover {
+        color: var(--o-color-primary1);
+      }
+    }
+
+    .locale-tag {
+      position: absolute;
+      font-size: 10px;
+      height: 12px;
+      width: 12px;
+      background-color: var(--o-color-fill2);
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      left: 12px;
+      top: 11px;
+
+      &.is-en {
+        width: 16px;
+      }
+    }
+  }
+  .list {
+    cursor: pointer;
+    border-radius: var(--o-radius_control-xs);
+    padding: var(--o-gap-2) var(--o-gap-4);
+    width: 136px;
+  }
+}
+
+.o-dropdown {
+  height: 100%;
+}
+.o-dropdown-item {
+  background: var(--o-color-fill2);
+  cursor: pointer;
+  border-radius: var(--o-radius_control-xs);
+  padding: var(--o-gap-1);
+  min-width: 144px;
+  height: 40px;
+  color: var(--o-color-info1);
+
+  @include hover {
+    background: var(--o-color-control2-light);
+  }
+
+  &.is-active {
+    color: var(--o-color-primary1);
+    background: var(--o-color-control3-light);
+  }
+}
+:deep(.dropdown) {
+  --dropdown-list-radius: var(--o-radius-s);
+}
+
+.mobile-change-language {
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+  span {
+    color: var(--o-color-info1);
+    text-align: center;
+    @include text1;
+    cursor: pointer;
+    &.active {
+      color: var(--o-color-primary1);
+      font-weight: 500;
+    }
+    &:not(:last-child) {
+      margin-right: var(--o-gap-3);
+      @include respond-to('phone') {
+        margin-right: var(--o-gap-2);
+      }
+
+      &:after {
+        content: '|';
+        margin-left: var(--o-gap-3);
+        color: var(--o-color-info1);
+
+        @include respond-to('phone') {
+          margin-left: var(--o-gap-2);
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ app/.vitepress/src/components/header/ItemTheme.vue
@@ -0,0 +1,131 @@
+<script lang="ts" setup>
+import { computed, onMounted, watch } from 'vue';
+import { isClient, OIcon } from '@opensig/opendesign';
+import { ElSwitch } from 'element-plus';
+import { useAppearance } from '@/stores/common';
+
+import IconSun from '~icons/app/icon-sun.svg';
+import IconHeaderMoon from '~icons/app/icon-header-moon.svg';
+import { getCustomCookie, setCustomCookie } from '@/utils/cookie';
+
+
+// 风格切换
+const APPEARANCE_KEY = 'openGauss-theme-appearance';
+
+const commonStore = useAppearance();
+
+const isLight = computed(() => (commonStore.theme === 'light' ? true : false));
+
+const changeTheme = () => {
+  const theme = commonStore.theme === 'dark' ? 'light' : 'dark';
+  commonStore.theme = theme;
+  setCustomCookie(APPEARANCE_KEY, theme, 180, import.meta.env.VITE_COOKIE_DOMAIN);
+};
+
+const changeThemeMobile = () => {
+  setCustomCookie(APPEARANCE_KEY, commonStore.theme, 180, import.meta.env.VITE_COOKIE_DOMAIN);
+};
+
+onMounted(() => {
+  let theme;
+  if (!getCustomCookie(APPEARANCE_KEY)) {
+    const prefereDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    theme = prefereDark ? 'dark' : 'light';
+  } else {
+    theme = getCustomCookie(APPEARANCE_KEY);
+  }
+  commonStore.theme = theme === 'dark' ? 'dark' : 'light';
+});
+
+watch(
+  () => {
+    return commonStore.theme;
+  },
+  (val) => {
+    if (isClient) {
+      const documentElement = document.documentElement;
+      val === 'light' && documentElement.removeAttribute('data-o-theme');
+      val === 'dark' && documentElement.setAttribute('data-o-theme', 'dark');
+      val === 'light' && documentElement.classList.remove('dark');
+      val === 'dark' && documentElement.classList.add('dark');
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<template>
+  <div class="theme-box">
+    <div class="theme-box-pc" @click="changeTheme">
+      <OIcon class="icon">
+        <IconHeaderMoon v-if="isLight" />
+        <IconSun v-else />
+      </OIcon>
+    </div>
+    <div class="theme-box-mobile">
+      <ElSwitch
+        v-model="commonStore.theme"
+        active-value="dark"
+        inactive-value="light"
+        inline-prompt
+        active-color="#7d32ea"
+        :active-icon="IconSun"
+        :inactive-icon="IconHeaderMoon"
+        @click="changeThemeMobile"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.theme-box {
+  .theme-box-pc {
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    display: flex;
+    align-items: center;
+
+    .icon {
+      font-size: 24px;
+      color: var(--o-color-info1);
+
+      @include hover {
+        color: var(--o-color-primary1);
+      }
+    }
+
+    @include respond-to('<=pad_v') {
+      display: none;
+    }
+  }
+
+  .theme-box-mobile {
+    margin-top: 24px;
+    display: none;
+    @include respond-to('<=pad_v') {
+      display: flex;
+      :deep(.o-switch) {
+        background: none;
+        display: flex;
+        align-items: center;
+      }
+    }
+  }
+}
+
+@include in-dark {
+  .icon {
+    color: var(--o-color-info1);
+  }
+}
+
+:deep(.el-switch .el-switch__core .el-switch__inner) {
+  .el-icon {
+    font-size: 20px;
+    color: var(--o-color-info1);
+  }
+}
+</style>
